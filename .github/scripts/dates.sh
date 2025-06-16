42570f802b7bf60b2e2d60ae6fa3e33a9b70f5d3
Nick <noobping@users.noreply.github.com>
2025-06-16 21:28:20 +0200

    Update dates.sh

--- .github/scripts/dates.sh
+++ .github/scripts/dates.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Add date to TOML front matter if it does not exist. Only for Markdown files, ignoring _index.md files.
-find content/ -type f -name '*.md' ! -name '_index.md' | while read -r file; do
+# Add date to TOML front matter if it does not exist. Only for Markdown files, ignoring _*.md files.
+find content/ -type f -name '*.md' ! -name '_*.md' | while read -r file; do
     if grep -q '^+++ *$' "$file"; then
         # Check if "date =" is already in front matter
         if ! awk '/^\+\+\+/{i++} i==1 && /^date *=/ {found=1; exit} END{exit !found}' "$file"; then
